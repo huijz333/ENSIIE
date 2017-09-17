3f3785d813454a1a97dd665ad2f925b1d455b6ad
rpereira <cedquen@hotmail.fr>
2017-09-17 17:10:34 +0200

    javascript particles

---
+++ particles/utils.js
@@ -0,0 +1,11 @@
+function sleep(ms) {
+	return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+function getUrlVars() {
+	var vars = {};
+	var parts = window.location.href.replace(/[?&]+([^=&]+)=([^&]*)/gi, function(m,key,value) {
+		vars[key] = value;
+	});
+	return (vars);
+}
