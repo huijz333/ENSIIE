de62060300d67c77ac6df362f1020e8e596d0b45
rpereira <cedquen@hotmail.fr>
2017-10-04 08:44:09 +0200

    tp2

---
+++ ProgrammationImperative/tp2/includes/main.h
@@ -0,0 +1,13 @@
+#ifndef MAIN_H
+# define MAIN_H
+
+# include <strings.h>
+# include <stdio.h>
+# include "array_list.h"
+
+typedef struct	s_question {
+	char 		* label;
+	t_function	function;
+}		t_question;
+
+#endif
