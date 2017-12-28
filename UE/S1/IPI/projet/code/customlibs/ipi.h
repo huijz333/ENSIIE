40a35986104c529c65851ec76bde592dacad0fe0
rpereira <cedquen@hotmail.fr>
2017-12-28 02:04:33 +0100

    rapport.pdf

--- UE/S1/IPI/projet/code/customlibs/ipi.h
+++ UE/S1/IPI/projet/code/customlibs/ipi.h
@@ -29,10 +29,11 @@
 	 *	NB: gcc [FLAGS] -D MAX_NODES=50
 	 */
 #	define MAX_NODES (1000 * 1000)
-#	pragma message("\nValue par défaut utilisée pour MAX_NODES (= 1000 * 1000)\n"\
+/*#	pragma message("\nValue par défaut utilisée pour MAX_NODES (= 1000 * 1000)\n"\
 			"Vous pouvez definir une valeur plus basse pour optimiser "\
 			"l'usage mémoire\nou plus grande si l'entrée contient "\
 			"plus de 1000 * 1000 sommets.\ne.g: gcc [FLAGS] -D MAX_NODES=50")
+*/
 # endif
 
 # if MAX_NODES < UCHAR_MAX
