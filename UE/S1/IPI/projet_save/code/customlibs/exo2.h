ca3e48ed19409f2141babab50bf02c8b77d4b07b
rpereira <cedquen@hotmail.fr>
2018-01-06 20:08:21 +0100

    removed useless files

--- UE/S1/IPI/projet_save/code/customlibs/exo2.h
+++
@@ -1,7 +0,0 @@
-#ifndef EXO2_H
-# define EXO2_H
-
-# include "ipi.h"	/* constantes */
-# include "dijkstra.h"
-
-#endif
--- UE/S1/IPI/projet_save/code/customlibs/exo3.h
+++
@@ -1,9 +0,0 @@
-#ifndef EXO3_H
-# define EXO3_H
-
-# include "ipi.h"
-# include "hmap.h"
-# include "lab.h"
-# include "bitmap.h"
-
-#endif
