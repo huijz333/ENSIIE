d3d0aafda1fcf2e4759ec1eea9974f2f30c1bc9b
rpereira <romain.pereira@ensiie.fr>
2018-11-21 15:43:55 +0100

    dispatcher

---
+++ UE/S3/microarchi/bus_ia/dispacher.vhd
@@ -0,0 +1,172 @@
+-------------------------------------------------------------------------------
+--
+-- Ce bloc interprete des messages correspond aux commandes processeurs
+--
+--	Il prends un message en paramètre au format
+--		(3 bits ; 21 bits)
+--		(  cmd  ;   msg  )
+--
+--		- 'cmd' la commande a executé
+--		- 'msg' le message de la commande
+--
+--	TODO comment
+--
+--
+-------------------------------------------------------------------------------
+
+library IEEE;
+use IEEE.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+ENTITY dispacher IS
+	PORT(
+			-- la master clock et le reset
+		    clk   : in  STD_LOGIC;
+		    reset : in  STD_LOGIC;
+			
+			-- message en entrée
+			busmsg : in STD_LOGIC_VECTOR(23 downto 0);
+
+		    -- les 32 valeurs du 7 segments configurées (7 * 32 = 224)
+		    busSS : out STD_LOGIC_VECTOR(223 downto 0);
+
+		    -- N : nombre de valeurs configurées à interpréter
+		    busN : out STD_LOGIC_VECTOR(5 downto 0);
+
+		    -- N_clock : nombre de clock à attendre pour générer un tick
+		    busNClock : out STD_LOGIC_VECTOR(20 downto 0)
+	    );
+END dispacher;
+
+ARCHITECTURE montage OF dispacher IS
+
+	-------------------------------------------------------------------------------
+	--  Partie Opérative
+	-------------------------------------------------------------------------------
+
+	-- Commande pour 'busmsg'
+	-- 	SEND => envoit du message
+	-- 	NOOP => envoit d'un buffer 0
+	TYPE T_CMD IS (READ, DISPATCH);
+	signal cmd : T_CMD;
+
+	-- le registre de stockage des données du dernier message reçu
+	signal R_Msg : STD_LOGIC_VECTOR(23 downto 0);
+
+	-- check (ON ou OFF) pour que le processeur envoit un message au PC
+	-- tous les 1000 tickets de H100
+	signal R_TICK1000 : STD_LOGIC;
+
+	-- N : nombre de valeurs configurées à interpréter (2^5 = 32 ; 2^6 = 64)
+	signal R_N : STD_LOGIC_VECTOR(5 downto 0);
+
+	-- registre stockant les 32 valeurs du 7 segments configurées (7 * 32 = 224)
+	signal R_SS : STD_LOGIC_VECTOR(223 downto 0);
+
+	-- registre stockant V (nombre de master clock à attendre avant de générer un tick)
+	signal R_N_CLOCK : STD_LOGIC_VECTOR(20 downto 0);
+
+	-------------------------------------------------------------------------------
+	-- Partie Contrôle
+	-------------------------------------------------------------------------------
+
+	type STATE_TYPE is (
+		ST_READ, ST_DISPATCH
+	);
+	signal state : STATE_TYPE;
+
+BEGIN
+
+	-------------------------------------------------------------------------------
+	--  Partie Opérative
+	-------------------------------------------------------------------------------
+
+	PROCESS (reset, clk)
+	BEGIN
+		-- si on reset
+		IF reset = '1' THEN
+			-- 100 ticks par seconde (500 est en kilo-clock)
+			R_N_CLOCK <= STD_LOGIC_VECTOR(to_unsigned(5000, 20));
+
+			-- configure le serpentin pour qu'il soit vide
+			R_N  <= STD_LOGIC_VECTOR(to_unsigned(32, 6));
+			R_SS <= (others => '0');
+
+			-- desactive le check TICK1000
+			R_TICK1000 <= '0';
+
+		ELSIF clk'event AND clk = '1' THEN
+
+			-- stocke le message
+			IF cmd = READ THEN
+				R_Msg <= busmsg;
+
+			-- dispatch le message
+			ELSIF cmd = DISPATCH THEN
+
+				-- switch l'id du message
+				CASE R_Msg(23 downto 21) IS
+
+					-- si 0 == NOOP
+					WHEN "000" =>
+						-- ne rien faire
+
+					-- hinit(n_clock)
+					WHEN "001" =>
+						R_N_CLOCK <= R_Msg(20 downto 0);
+
+					-- h-check-on()
+					WHEN "010" =>
+						R_TICK1000 <= '1';
+
+					-- h-check-off()
+					WHEN "011" =>
+						R_TICK1000 <= '0';
+
+					-- clr(s)
+					WHEN "100" =>
+						R_N  <= STD_LOGIC_VECTOR(to_unsigned(32, 6));
+						R_SS <= (others => '0');
+
+					-- si commande set-N(n)
+					WHEN "101" =>
+						R_N <= R_tft(5 downto 0);
+
+					-- si commande set-val(i, v)
+					WHEN "110" =>
+						-- TODO
+						-- R_SS((i + 1) * 7 - 1, i * 7) <= R_tft(6 downto 0);
+				END CASE;
+			END IF;
+		END IF;
+	END PROCESS;
+
+	busSS       <= R_SS;
+	busN        <= R_N;
+	busNClock   <= R_N_CLOCK;
+	busTICK1000 <= R_TICK1000;
+
+	-------------------------------------------------------------------------------
+	-- Partie Contrôle
+	-------------------------------------------------------------------------------
+
+	-- fonction de transitition    
+	PROCESS (reset,clk)
+	BEGIN
+		IF clk'event AND clk = '1' THEN
+			IF state = ST_READ THEN
+				state <= ST_DISPATCH;
+			ELSIF state = ST_DISPATCH THEN
+				state <= ST_READ;
+			END IF;
+		END IF;
+	END PROCESS;
+
+	-- fonction de sortie    
+	WITH state SELECT cmd <=
+		READ       WHEN   ST_READ,
+		DISPATCH   WHEN   ST_DISPATCH
+	; 
+
+end montage;
+
--- UE/S3/microarchi/bus_ia/wrapper.vhd
+++ UE/S3/microarchi/bus_ia/wrapper.vhd
@@ -12,9 +12,11 @@
 --
 -- Du coté busin, il suit le protocole "poignée de main" (signaux: busin, 
 --   busin_valid, busin_eated).
+--
 -- Du coté busout, il suit le protocole "poignée de main" (signaux: busout, 
 --   busout_valid, busout_eated).
--- Du coté busv, la valeur est transmise sans check
+--
+-- Du coté busmsg, la valeur du message est transmise
 -------------------------------------------------------------------------------
 
 library IEEE;
@@ -39,14 +41,8 @@ ENTITY wrapper IS
 		    busout_valid : out STD_LOGIC;
 		    busout_eated : in  STD_LOGIC;
 
-		    -- les 32 valeurs du 7 segments configurées (7 * 32 = 224)
-		    busSS : out STD_LOGIC_VECTOR(223 downto 0);
-
-		    -- N : nombre de valeurs configurées à interpréter
-		    busN : out STD_LOGIC_VECTOR(5 downto 0);
-
-		    -- N_clock : nombre de clock à attendre pour générer un tick
-		    busNClock : out STD_LOGIC_VECTOR(20 downto 0)
+		    -- le bus qui transmet le message au processeur 7 segments
+		    busmsg : out STD_LOGIC_VECTOR(23 downto 0)
 	    );
 END wrapper;
 
@@ -55,35 +51,23 @@ ARCHITECTURE montage OF wrapper IS
 	-------------------------------------------------------------------------------
 	--  Partie Opérative
 	-------------------------------------------------------------------------------
+
 	-- Registre de transfert entre busin et busout
 	type T_CMD_tft is (INIT, NOOP);
 	signal CMD_tft :  T_CMD_tft ; 
 	signal R_tft   :  STD_LOGIC_VECTOR (43 downto 0);
 
-	-- Registre de stockage des données du dernier message reçu à destination de ce processeur
-	TYPE T_CMD_Msg IS (LOAD, NOOP);
+	-- Commande pour 'busmsg'
+	-- 	SEND => envoit du message
+	-- 	NOOP => envoit d'un buffer 0
+	TYPE T_CMD_Msg IS (SEND, NOOP);
 	signal CMD_Msg : T_CMD_Msg ;
-
-	-- check (ON ou OFF) pour que le processeur envoit un message au PC
-	-- tous les 1000 tickets de H100
-	signal R_TICK1000 : STD_LOGIC;
-
-	-- N : nombre de valeurs configurées à interpréter (2^5 = 32 ; 2^6 = 64)
-	signal R_N : STD_LOGIC_VECTOR(5 downto 0);
-
-	-- registre stockant les 32 valeurs du 7 segments configurées (7 * 32 = 224)
-	signal R_SS : STD_LOGIC_VECTOR(223 downto 0);
-
-	-- registre stockant V (nombre de master clock à attendre avant de générer un tick)
-	signal R_N_CLOCK : STD_LOGIC_VECTOR(20 downto 0);
+	signal R_Msg   :  STD_LOGIC_VECTOR (23 downto 0);
 
 	-------------------------------------------------------------------------------
 	-- Partie Contrôle
 	-------------------------------------------------------------------------------
 	-- adresse destination
-	alias busin_addrdest : STD_LOGIC_VECTOR(7 downto 0) is busin(31 downto 24);
-
-	-- valeur destination
 	alias busin_addrdest : STD_LOGIC_VECTOR(7 downto 0) is busin(31 downto 24);
 
 	type STATE_TYPE is (
@@ -100,18 +84,7 @@ BEGIN
 	PROCESS (reset, clk)
 	BEGIN
 		-- si on reset
-		IF reset = '1' THEN
-			-- 100 ticks par seconde (500 est en kilo-clock)
-			R_N_CLOCK <= STD_LOGIC_VECTOR(to_unsigned(5000, 20));
-
-			-- configure le serpentin pour qu'il soit vide
-			R_N  <= STD_LOGIC_VECTOR(to_unsigned(32, 6));
-			R_SS <= (others => '0');
-
-			-- desactive le check TICK1000
-			R_TICK1000 <= '0';
-
-		ELSIF clk'event AND clk = '1' THEN
+		IF clk'event AND clk = '1' THEN
 
 			-- commandes de transfert bus ia
 			-- si l'on doit lire le message, on le stocke
@@ -120,48 +93,19 @@ BEGIN
 				R_tft <= busin ;
 			END IF;
 
-			-- si le message doit être chargé (<=> qu'il nous est destiné)
-			-- on met à jour les registre de stockages
+			-- si le message doit être chargé
+			-- on met à jour le registre de stockage
 			IF CMD_Msg = LOAD THEN
-
-				-- switch l'id du message
-				CASE R_tft(23 downto 21) IS
-
-					-- hinit(n_clock)
-					WHEN "000" =>
-						R_N_CLOCK <= R_tft(20 downto 0);
-
-					-- h-check-on()
-					WHEN "001" =>
-						R_TICK1000 <= '1';
-
-					-- h-check-off()
-					WHEN "010" =>
-						R_TICK1000 <= '0';
-
-					-- clr(s)
-					WHEN "011" =>
-						R_N  <= STD_LOGIC_VECTOR(to_unsigned(32, 6));
-						R_SS <= (others => '0');
-
-					-- si commande set-N(n)
-					WHEN "100" =>
-						R_N <= R_tft(5 downto 0);
-
-					-- si commande set-val(i, v)
-					WHEN "101" =>
-						-- TODO
-						-- R_SS((i + 1) * 7 - 1, i * 7) <= R_tft(6 downto 0);
-
+				R_Msg <= R_tft(23 downto 0);
+			-- sinon (== commande NOOP), on met le registre à 0
+			ELSIF
+				R_Msg <= (others => '0');
 			END IF ;
 		END IF;
 	END PROCESS;
 
-	busout      <= R_tft;
-	busSS       <= R_SS;
-	busN        <= R_N;
-	busNClock   <= R_N_CLOCK;
-	busTICK1000 <= R_TICK1000;
+	busou  <= R_tft;
+	busmsg <= R_Msg;
 
 	-------------------------------------------------------------------------------
 	-- Partie Contrôle
@@ -198,7 +142,7 @@ BEGIN
 		END IF;
 	END PROCESS;
 
-		-- fonction de sortie    
+	-- fonction de sortie    
 	WITH state SELECT busin_eated <=
 		'1'    WHEN   ST_READ_BUSIN,
 		'0'    WHEN   others
