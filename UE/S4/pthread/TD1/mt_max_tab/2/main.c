5e994d5d454533af74ab893096551acb8ebce4ca
Romain PEREIRA <romain.pereira@pc227-05.pedago.ensiie.fr>
2019-02-08 17:27:29 +0100

    barriere

--- UE/S4/pthread/TD1/mt_max_tab/2/main.c
+++ UE/S4/pthread/TD1/mt_max_tab/2/main.c
@@ -4,14 +4,26 @@
 # include <string.h>
 # include <time.h>
 
+/* valeur maximal dans le tableau */
+# define MAX_VAL (1000)
+
 /* nombre d'entier dans le tableau */
 # define N_INTEGERS (20)
 
 /* seed pour l'aléatoire */
 # define SEED (time(NULL))
 
+/* nombre total de threads */
 int nb_threads;
+
+/* tableau d'entiers aléatoires */
 int integers[N_INTEGERS];
+
+/* maximum global */
+volatile int m_global;
+
+/* mutex sur la variable "m_global" */
+pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
 
 /**
  * calcul le maximum local pour le thread numéro 'i'
@@ -23,14 +35,21 @@ static long localMax(long i) {
 	int bgn = i * integers_per_thread; /* debut local */
 	int end = (i == nb_threads - 1) ? N_INTEGERS : (i + 1) * integers_per_thread; /* fin local */
 	/* printf("%d to %d\n", bgn, end); */
-	int m = integers[bgn];
+	int m_local = integers[bgn];
 	int j;
 	for (j = bgn + 1 ; j < end ; j++) {
-		if (integers[j] > m) {
-			m = integers[j];
+		if (m_local < integers[j] ) {
+			m_local = integers[j];
 		}
 	}
-	return (long)m;
+
+	pthread_mutex_lock(&mutex);
+	if (m_global < m_local) {
+		m_global = m_local;
+	}
+	pthread_mutex_unlock(&mutex);
+
+	return (long)m_local;
 }
 
 int main(int argc, char ** argv) {
@@ -45,8 +64,10 @@ int main(int argc, char ** argv) {
 	srand(SEED);
 	long i;
 	for (i = 0 ; i < N_INTEGERS ; i++) {
-		integers[i] = rand();
+		integers[i] = rand() % MAX_VAL;
 	}
+	/* initialise la valeur max */
+	m_global = integers[0];
 
 	/* création des threads */
 	pthread_t * thrds = (pthread_t *) malloc(nb_threads * sizeof(pthread_t));
@@ -57,23 +78,19 @@ int main(int argc, char ** argv) {
 		}
 	}
 
-	/* join sur les threads + calcul du maximum global à partir des maximums locaux */
-	int m = integers[0];
+	/* join sur les threads */
 	for (i = 0 ; i < nb_threads ; i++) {
 		pthread_t * thrd = thrds + i;
 		if (!thrd) {
 			continue ;
 		}
-		long m_local;
-		pthread_join(*thrd, (void **)&m_local);
-		/* printf("Maximum local: %ld\n", m_local); */
-		if ((int)m_local > m) {
-			m = m_local;
-		}
+		void * unused;
+		pthread_join(*thrd, &unused);
 	}
-	printf("Maximum global: %d\n", m);
 
 	free(thrds);
+	pthread_mutex_destroy(&mutex);
+	printf("Maximum global: %d\n", m_global);
 
 	return 0;
 }
